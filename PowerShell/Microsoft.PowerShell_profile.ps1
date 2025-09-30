53581e4079009cd7144b7158c82a9dcef9bdc8e6
Gustavo Azevedo <gustavo.hzevedo@gmail.com>
2025-09-30 17:59:02 -0300

    Update Microsoft.PowerShell_profile.ps1

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,14 +1,9 @@
 
-#34de4b3d-13a8-4540-b76d-b9e8d3851756 PowerToys CommandNotFound module
-
-Import-Module "C:\Program Files\PowerToys\WinUI3Apps\..\WinGetCommandNotFound.psd1"
-#34de4b3d-13a8-4540-b76d-b9e8d3851756
+#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module (deferred in profile.ps1 OnIdle)
+# Import is handled lazily to improve startup time.
 # Import the Chocolatey Profile that contains the necessary code to enable
 # tab-completions to function for `choco`.
 # Be aware that if you are missing these lines from your profile, tab completion
 # for `choco` will not function.
 # See https://ch0.co/tab-completion for details.
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
+# Chocolatey profile import is deferred in profile.ps1 idle handler
